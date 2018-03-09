e67ac47cb7fba29eee619efb09957c8fee301d67
u1188 <u1188@Sreehari-new>
2018-03-09 17:13:51 +0530

    ok

--- src/me/harisree/aspose/utils/AsposeTest.java
+++ src/me/harisree/aspose/utils/AsposeTest.java
@@ -1,11 +1,23 @@
-package me.harisree.aspose;
+package me.harisree.aspose.utils;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.aspose.tasks.Asn;
+import com.aspose.tasks.ChildTasksCollector;
 import com.aspose.tasks.License;
 import com.aspose.tasks.Prj;
 import com.aspose.tasks.Project;
+import com.aspose.tasks.Resource;
+import com.aspose.tasks.ResourceAssignment;
+import com.aspose.tasks.ResourceAssignmentCollection;
+import com.aspose.tasks.Rsc;
 import com.aspose.tasks.SaveFileFormat;
+import com.aspose.tasks.Task;
+import com.aspose.tasks.TaskUtils;
+import com.aspose.tasks.Tsk;
+
 
 public class AsposeTest {
 	
@@ -31,6 +43,18 @@ public class AsposeTest {
 		//System.out.println("Calendar : " + project.get(Prj.AUTHOR));
 		
 		//project.save(HOME_DIRECTORY + "project3.mpp", SaveFileFormat.MPP);
+		
+		for (Task task : getTasksForBiweeklyUpdate(project)) {
+			
+			//System.out.println("Task Name: " + task.get(Tsk.NAME));
+			
+			for(ResourceAssignment assignment : getResourcesForTask(task)) {
+				
+				System.out.println("Task Name: " + task.get(Tsk.NAME) + "[" + assignment.get(Asn.UNITS) +" " +assignment.get(Rsc.MATERIAL_LABEL)+ "]");
+				
+			}
+
+		}
 		
 	}
 	
@@ -71,6 +95,30 @@ public class AsposeTest {
 	public static void saveAsMpp(Project project, String newFileName) {
 		
 		project.save(HOME_DIRECTORY + newFileName, SaveFileFormat.MPP);
+		
+	}
+	
+	public static List<Task> getTasksForBiweeklyUpdate(Project project) {
+		
+		ChildTasksCollector collector = new ChildTasksCollector();
+		
+		TaskUtils.apply(project.getRootTask(), collector, 0);
+		
+		return collector.getTasks();
+		
+	}
+	
+	public static List<ResourceAssignment> getResourcesForTask(Task task) {
+		
+		List<ResourceAssignment> resourceList = new ArrayList<>();
+		
+		for(ResourceAssignment ass : task.getAssignments()){
+			
+			resourceList.add(ass);
+		
+		}
+		
+		return resourceList;
 		
 	}
 
