4b2d0f98b863d8650ba30c851086ce012f73eebd
u1188 <u1188@Sreehari-new>
2018-03-12 18:59:59 +0530

    Writing to files

--- src/me/harisree/aspose/utils/AsposeTest.java
+++ src/me/harisree/aspose/utils/AsposeTest.java
@@ -2,28 +2,37 @@ package me.harisree.aspose.utils;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import com.aspose.tasks.Asn;
 import com.aspose.tasks.ChildTasksCollector;
 import com.aspose.tasks.License;
 import com.aspose.tasks.Prj;
 import com.aspose.tasks.Project;
+import com.aspose.tasks.ProjectView;
 import com.aspose.tasks.Resource;
 import com.aspose.tasks.ResourceAssignment;
 import com.aspose.tasks.ResourceAssignmentCollection;
 import com.aspose.tasks.Rsc;
 import com.aspose.tasks.SaveFileFormat;
 import com.aspose.tasks.Task;
+import com.aspose.tasks.TaskUsageView;
 import com.aspose.tasks.TaskUtils;
+import com.aspose.tasks.TimeUnitType;
+import com.aspose.tasks.TimephasedData;
+import com.aspose.tasks.TimephasedDataCollection;
+import com.aspose.tasks.TimephasedDataType;
 import com.aspose.tasks.Tsk;
+import com.aspose.tasks.WorkContourType;
 
 
 public class AsposeTest {
 	
 	private static final String LICENSE_PATH = "/home/u1188/ddfs/aspose/Aspose.Tasks.lic";
 	private static final String HOME_DIRECTORY = "/home/u1188/ddfs/aspose/";
-	private static final String SAMPLE_MPP = "project3.mpp";
+	private static final String SAMPLE_MPP = "Project1_2007.mpp";
 	
 	
 	public static void main(String[] args) {
@@ -36,25 +45,53 @@ public class AsposeTest {
 		
 		Project project = openProject(HOME_DIRECTORY, SAMPLE_MPP);
 		
-		System.out.println("Calendar : " + project.get(Prj.AUTHOR));
-		
-		//project.set(Prj.AUTHOR,"Sreehari B S");
-		
-		//System.out.println("Calendar : " + project.get(Prj.AUTHOR));
-		
-		//project.save(HOME_DIRECTORY + "project3.mpp", SaveFileFormat.MPP);
-		
-		for (Task task : getTasksForBiweeklyUpdate(project)) {
+		ChildTasksCollector collector = new ChildTasksCollector();
+		TaskUtils.apply(project.getRootTask(),collector,0);
+		List<Task> tasks =  collector.getTasks();
+		for(Task tsk : tasks)
+		{
+			//System.out.println(tsk.get(Tsk.NAME));
 			
-			//System.out.println("Task Name: " + task.get(Tsk.NAME));
+		    /*if(tsk.get(Tsk.UID) == 2 ) {
+		    	tsk.set(Tsk.PERCENT_COMPLETE, 40);
+		    }*/
 			
-			for(ResourceAssignment assignment : getResourcesForTask(task)) {
+			System.out.println(tsk.toString());
+			
+			for(ResourceAssignment assign : tsk.getAssignments()) {
 				
-				System.out.println("Task Name: " + task.get(Tsk.NAME) + "[" + assignment.get(Asn.UNITS) +" " +assignment.get(Rsc.MATERIAL_LABEL)+ "]");
+				System.out.println(assign.get(Asn.UID));
 				
 			}
-
+			
 		}
+		
+/*		for(ResourceAssignment assn :  project.getResourceAssignments()) {
+			
+			System.out.println(assn.toString());
+			
+		}*/
+		
+		ResourceAssignment assn =  project.getResourceAssignments().getByUid(18);
+		
+		assn.set(Asn.WORK_CONTOUR,WorkContourType.Contoured);
+
+		TimephasedData td = new TimephasedData();
+		td.setUid(18);
+	    td.setStart(assn.get(Asn.START));
+	    td.setFinish(new Date(assn.get(Asn.START).getTime()+86400000));
+	    td.setValue("2");
+	    td.setUnit(TimeUnitType.Hour);
+	    td.setTimephasedDataType(TimephasedDataType.AssignmentRemainingWork);
+	    assn.getTimephasedData().add(td);
+		
+		project.recalculate();
+		project.save(HOME_DIRECTORY+"Project1_2007_out.mpp", SaveFileFormat.MPP);
+		
+		System.out.println("Done");
+		
+		
+		//saveAsMpp(project, "project_updated.mpp");
 		
 	}
 	
@@ -94,7 +131,7 @@ public class AsposeTest {
 	
 	public static void saveAsMpp(Project project, String newFileName) {
 		
-		project.save(HOME_DIRECTORY + newFileName, SaveFileFormat.MPP);
+		project.save(HOME_DIRECTORY + newFileName, SaveFileFormat.XML);
 		
 	}
 	
